2df195d3768cb9fc1d59b3c6faadec3cd4525274
Elsayed Ahmed <elsayed.ahmed.elwasefy@gmail.com>
2021-03-09 02:23:30 +0200

    Change Screen shot

--- src/server.ts
+++ src/server.ts
@@ -13,30 +13,19 @@ import {filterImageFromURL, deleteLocalFiles} from './util/util';
   // Use the body parser middleware for post requests
   app.use(bodyParser.json());
 
-  // @TODO1 IMPLEMENT A RESTFUL ENDPOINT
-  // GET /filteredimage?image_url={{URL}}
-  // endpoint to filter an image from a public url.
-  // IT SHOULD
-  //    1
-  //    1. validate the image_url query
-  //    2. call filterImageFromURL(image_url) to filter the image
-  //    3. send the resulting file in the response
-  //    4. deletes any files on the server on finish of the response
-  // QUERY PARAMATERS
-  //    image_url: URL of a publicly accessible image
-  // RETURNS
-  //   the filtered image file [!!TIP res.sendFile(filteredpath); might be useful]
-
-  app.get( "/filteredimage", async ( req: any, res:any ) => {
-    let image_url = req.query.image_url
-
-    if (image_url === undefined) {
-      res.send('image url not found')
+  app.get("/filteredimage/", async (req, res) =>{
+    try {
+      let {image_url} = req.query;
+      if(!image_url){
+        return res.status(400).send("bad request!");
+      }
+      const path = await filterImageFromURL(image_url);
+      console.log('---------------------', path, '--------------------------');
+      res.sendFile(path);
+      res.on('finish', () => deleteLocalFiles([path]));
+    } catch {
+      return res.status(500).send({error: 'Unable to process your request'});
     }
-
-    const filteredpath = filterImageFromURL(image_url)
-
-    res.sendFile(filteredpath)
   });
   /**************************************************************************** */
 
